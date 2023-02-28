f6fdf98a3e686495209232f65acda91225ac9bf3
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-28 22:23:25 +0000

    Get the oldest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,12 @@ class DirectorsController < ApplicationController
     @list_of_directors = Director.all
     render({ :template => "director_template/index.html.erb"})
   end
+
+  def wisest
+
+    @oldest = Director.where.not({ :dob => nil}).order({ :dob => :asc}).at(0)
+
+    render({ :template => "director_template/eldest.html.erb"})
+  end
+
 end
