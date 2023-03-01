56264613059d3bb955301840e2e3efed19317b06
Mohammad Ali Karimi <ali216k@gmail.com>
2023-03-01 18:24:02 +0000

    Showing the details of directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,4 +11,12 @@ class DirectorsController < ApplicationController
     render({ :template => "director_template/eldest.html.erb"})
   end
 
+  def director_details
+
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id }).at(0)
+
+    render({ :template => "director_template/show.html.erb"})
+  end
+
 end
