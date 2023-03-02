6ad6050a6e8ceb3ace018eba01610bc5e811d66c
Mohammad Ali Karimi <ali216k@gmail.com>
2023-03-02 04:14:14 +0000

    Movies table and details totally work!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,17 @@
 class MoviesController < ApplicationController
+
   def lists
+
     @list_of_movies = Movie.all
     render({ :template => "movie_template/lists.html.erb"})
+  end
+
+  
+  def movie_details
+    @movie_id = params.fetch("movie_id")
+    @the_movie = Movie.where({ :id => @movie_id }).at(0)
+
+    render({ :template => "movie_template/movie.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   
   get("/movies", { :controller => "movies", :action => "lists" })
 
+  get("/movies/:movie_id", { :controller => "movies", :action => "movie_details" })
+
 end
